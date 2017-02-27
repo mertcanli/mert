d36a18370129bf80d650a0c4a1e7f99eea977fea
Beykoz1 <beykoz1@beykoz1-imac.local>
2017-02-27 13:10:53 +0200

    Initial Commit

---
+++ mert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mert
+//
+//  Created by Beykoz1 on 27.02.2017.
+//  Copyright © 2017 Beykoz1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
